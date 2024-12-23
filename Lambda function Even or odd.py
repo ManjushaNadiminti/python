c0b2ce330d95fc610200a1a7240dba94639fc94f
ManjushaNadiminti <Nadimintimanjusha@gmail.com>
2024-12-23 16:37:11 +0530

    Create Lambda function Even or odd.py

---
+++ Lambda function Even or odd.py
@@ -0,0 +1,10 @@
+even = lambda x:x%2==0
+odd = lambda x:x%2!=0
+n = int(input("Enter number:"))
+print("Even:",even(n))
+print("Odd:",odd(n))
+
+OUTPUT:
+       Enter number:6
+        Even: True
+        Odd: False
